e0bda7a9c3e80e4cfd883d488a17f460c7c2a8a3
Agata <agata@agata.pl>
2017-02-06 16:05:50 +0100

    Instance of User

---
+++ Tests/User_test.php
@@ -0,0 +1,17 @@
+<?php
+
+include "../Class/User.php";
+
+$oUser = new User();
+var_dump($oUser);
+
+//Back later
+if ($oUser instanceof User){
+    echo "Instance of User";
+}else{
+    echo "Instance is not User";
+    
+}
+
+//TODO test get and set functions
+//TODO Write funciotns to tests
