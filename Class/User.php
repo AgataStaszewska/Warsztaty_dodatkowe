4307bd81bc588a9ae281c2b386b733aacd7000fb
Agata <agata@agata.pl>
2017-02-06 15:46:58 +0100

    Klasa User przed testami

---
+++ Class/User.php
@@ -0,0 +1,76 @@
+<?php
+
+//Class represents one user
+
+    class User{
+        
+        private $id;
+        private $address;
+        private $name;
+        private $surname;
+        private $credits;
+        private $hashedPassword;
+        
+        public function __construct(){
+            $this->id = -1;
+            $this->address = "";
+            $this->name = "";
+            $this->surname = "";
+            $this->credits = null;  //Bo credits jest integerem, wiec nie mozemy dac mu pustego stringa;
+            $this->hashedPassword = "";
+            
+        }
+        
+        public function getId(){
+            return $this->id;
+        }      
+        
+        public function getAddress(){
+            return $this->address;
+        }
+        
+        public function setAddress($address){
+            $this->address = $address;
+            return true;
+        }
+        
+        public function getName(){
+            return $this->name;
+        }
+        
+        public function setName($name){
+            $this->name = $name;
+            return true;
+        }
+        
+        public function getSurname(){
+            return $this->surname;
+        }
+        
+        public function setSurname($surname){
+            $this->surname = $surname;
+            return true;
+        }
+        
+        public function getCredits(){
+            return $this->credits;
+        }
+        
+        public function setCredits($credits){
+            $this->credits = $credits;
+            return true;
+        }
+        
+        public function getHashedPassword($hashedPassword){
+             return $this->hashedPassword;
+        }
+        
+        public function setHashedPassword($password){
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+            $this->hashedPassword = $hashedPassword;
+            return true;
+       
+        }
+           
+    }
+    
